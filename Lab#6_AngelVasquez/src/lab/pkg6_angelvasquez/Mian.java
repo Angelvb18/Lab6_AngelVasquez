6e119b531044bb285b256cec799eecab293a9c1e
Angelvb18 <angeljosevb12@gmail.com>
2019-11-22 15:35:02 -0600

    Correcion de tabla

--- Lab#6_AngelVasquez/src/lab/pkg6_angelvasquez/Mian.java
+++ Lab#6_AngelVasquez/src/lab/pkg6_angelvasquez/Mian.java
@@ -43,14 +43,17 @@ public class Mian extends javax.swing.JFrame {
 
     public void Actualizar_Tabla() {
         jTable_Productos.setModel(new javax.swing.table.DefaultTableModel(
-                new Object[][]{},
-                new String[]{
-                    "Codigo", "Nombre", "Cantidadad", "Fecha Vencimiento", "Colorante"
-                }
+            new Object [][] {
+
+            },
+            new String [] {
+                "Codigo", "Nombre", "Cantidadad", "Fecha Vencimiento", "Colorante", "Azucar", "Alcohol", "Nacional o no", "Lote", "Perecio"
+            }
         ));
         DefaultTableModel modelot = (DefaultTableModel) jTable_Productos.getModel();
         for (int i = 0; i < prductos.size(); i++) {
-            Object[] Nrow = {prductos.get(i).getCodigo(), prductos.get(i).getNombre(), prductos.get(i).getCant(), prductos.get(i).getFecha_vencimiento(), prductos.get(i).getColorante()};
+            Object[] Nrow = {prductos.get(i).getCodigo(), prductos.get(i).getNombre(), prductos.get(i).getCant(), prductos.get(i).getFecha_vencimiento(), prductos.get(i).getColorante(),prductos.get(i).getCantonz()+"oz",prductos.get(i).getCantalchol()+"%",
+            prductos.get(i).getNacional(),prductos.get(i).getNumlote(),prductos.get(i).getPrecio()};
             modelot.addRow(Nrow);
         }
         jTable_Productos.setModel(modelot);
@@ -97,11 +100,14 @@ public class Mian extends javax.swing.JFrame {
         jMenuItem3 = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable_Productos = new javax.swing.JTable();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem7 = new javax.swing.JMenuItem();
 
         jLabel1.setText("Nombre");
 
@@ -310,6 +316,14 @@ public class Mian extends javax.swing.JFrame {
 
         pp_options.add(jMenu2);
 
+        jMenuItem6.setText("Agregar");
+        jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem6ActionPerformed(evt);
+            }
+        });
+        pp_options.add(jMenuItem6);
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jTable_Productos.setModel(new javax.swing.table.DefaultTableModel(
@@ -317,7 +331,7 @@ public class Mian extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Codigo", "Nombre", "Cantidadad", "Fecha Vencimiento", "Colorante"
+                "Codigo", "Nombre", "Cantidadad", "Fecha Vencimiento", "Colorante", "Azucar", "Alcohol", "Nacional o no", "Lote", "Perecio"
             }
         ));
         jTable_Productos.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -344,6 +358,13 @@ public class Mian extends javax.swing.JFrame {
 
         jMenuBar1.add(jMenu1);
 
+        jMenu3.setText("Mas Opciones");
+
+        jMenuItem7.setText("Cotización");
+        jMenu3.add(jMenuItem7);
+
+        jMenuBar1.add(jMenu3);
+
         setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -352,7 +373,7 @@ public class Mian extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 742, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1794, Short.MAX_VALUE)
                 .addGap(21, 21, 21))
         );
         layout.setVerticalGroup(
@@ -360,7 +381,7 @@ public class Mian extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(14, Short.MAX_VALUE))
+                .addContainerGap(106, Short.MAX_VALUE))
         );
 
         pack();
@@ -444,6 +465,14 @@ public class Mian extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jTable_ProductosMouseClicked
 
+    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
+        // TODO add your handling code here:
+         jd_crearProducto.pack();
+        jd_crearProducto.setModal(true);
+        jd_crearProducto.setLocationRelativeTo(this);
+        jd_crearProducto.setVisible(true);
+    }//GEN-LAST:event_jMenuItem6ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -492,12 +521,15 @@ public class Mian extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel9;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable_Productos;
